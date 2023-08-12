f33be09cdd31a12ffcf281ceaaf0cc9d34167208
Sorady <125706196+Sorady@users.noreply.github.com>
2023-08-13 00:42:03 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,36 +1,22 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from models.base_model import BaseModel, Base
-from os import getenv
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""This is the User Model module.
+
+Contains the User class that inherits from BaseModel.
+"""
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """ Represent a user for a MySQL database.
+class User(BaseModel):
+    """This class defines a User.
 
     Attributes:
-        __tablename__: represents the table name, users
-        email: (sqlalchemy String): The user's email address.
-        password (sqlalchemy String): The user's password.
-        first_name (sqlalchemy String): The user's first name.
-        last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The user-Place relationship.
-        reviews (sqlalchemy relationship): The user-Review relationship.
-
+        email (str): the user's email.
+        password (str): the user's password.
+        first_name (str): the user's first_name.
+        last_name (str): the user's last_name.
     """
 
-    __tablename__ = "users"
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = relationship("Place", backref="user", cascade="delete")
-        reviews = relationship("Review", backref="user", cascade="delete")
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
