85d34f083ffb130583d229e896bd22f75bb1e98a
Sorady <125706196+Sorady@users.noreply.github.com>
2023-08-13 03:27:29 +0300

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,100 +1,62 @@
 #!/usr/bin/python3
-"""
-Contains the TestStateDocs classes
-"""
-
-from datetime import datetime
-import inspect
-from models import state
-from models.base_model import BaseModel
-import pep8
+# test cases for base class
 import unittest
-State = state.State
-
-
-class TestStateDocs(unittest.TestCase):
-    """Tests to check the documentation and style of State class"""
-    @classmethod
-    def setUpClass(cls):
-        """Set up for the doc tests"""
-        cls.state_f = inspect.getmembers(State, inspect.isfunction)
-
-    def test_pep8_conformance_state(self):
-        """Test that models/state.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['models/state.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_pep8_conformance_test_state(self):
-        """Test that tests/test_models/test_state.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_models/test_state.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
-
-    def test_state_module_docstring(self):
-        """Test for the state.py module docstring"""
-        self.assertIsNot(state.__doc__, None,
-                         "state.py needs a docstring")
-        self.assertTrue(len(state.__doc__) >= 1,
-                        "state.py needs a docstring")
-
-    def test_state_class_docstring(self):
-        """Test for the State class docstring"""
-        self.assertIsNot(State.__doc__, None,
-                         "State class needs a docstring")
-        self.assertTrue(len(State.__doc__) >= 1,
-                        "State class needs a docstring")
-
-    def test_state_func_docstrings(self):
-        """Test for the presence of docstrings in State methods"""
-        for func in self.state_f:
-            self.assertIsNot(func[1].__doc__, None,
-                             "{:s} method needs a docstring".format(func[0]))
-            self.assertTrue(len(func[1].__doc__) >= 1,
-                            "{:s} method needs a docstring".format(func[0]))
+from models.base_model import BaseModel
+from models.state import State
+import models.base_model
+import models.state
+import inspect
+import datetime
+from time import sleep
 
 
 class TestState(unittest.TestCase):
-    """Test the State class"""
+    """ class to test city class """
+    def setUp(self):
+        """This method is called before each test method in the test class.
+        """
+        self.s = State()
 
-    def test_is_subclass(self):
-        """Test that State is a subclass of BaseModel"""
-        state = State()
-        self.assertIsInstance(state, BaseModel)
-        self.assertTrue(hasattr(state, "id"))
-        self.assertTrue(hasattr(state, "created_at"))
-        self.assertTrue(hasattr(state, "updated_at"))
+    def test_doc_state(self):
+        """ test_doc(self): to test if module and class has docs """
+        self.assertIsNotNone(State.__doc__, 'no docs for Base class')
+        self.assertIsNotNone(models.state.__doc__, 'no docs for module')
+        for name, method in inspect.getmembers(State, inspect.isfunction):
+            self.assertIsNotNone(method.__doc__, f"{name} has no docs")
 
-    def test_name_attr(self):
-        """Test that State has attribute name, and it's as an empty string"""
-        state = State()
-        self.assertTrue(hasattr(state, "name"))
-        self.assertEqual(state.name, "")
+    def test_init_state(self):
+        """ test instantiation of class """
+        self.assertEqual(type(self.s.id), str)
+        self.assertEqual(type(self.s.updated_at), datetime.datetime)
+        self.assertEqual(type(self.s.created_at), datetime.datetime)
 
-    def test_to_dict_creates_dict(self):
-        """test to_dict method creates a dictionary with proper attrs"""
-        s = State()
-        new_d = s.to_dict()
-        self.assertEqual(type(new_d), dict)
-        for attr in s.__dict__:
-            self.assertTrue(attr in new_d)
-            self.assertTrue("__class__" in new_d)
+    def test_save_state(self):
+        """ test State.save() """
+        current_updatedAt = self.s.updated_at
+        self.s.save()
+        self.assertNotEqual(current_updatedAt, self.s.updated_at)
 
-    def test_to_dict_values(self):
-        """test that values in dict returned from to_dict are correct"""
-        t_format = "%Y-%m-%dT%H:%M:%S.%f"
-        s = State()
-        new_d = s.to_dict()
-        self.assertEqual(new_d["__class__"], "State")
-        self.assertEqual(type(new_d["created_at"]), str)
-        self.assertEqual(type(new_d["updated_at"]), str)
-        self.assertEqual(new_d["created_at"], s.created_at.strftime(t_format))
-        self.assertEqual(new_d["updated_at"], s.updated_at.strftime(t_format))
+        """ test positional args """
+        with self.assertRaises(TypeError):
+            self.s.save(13)
 
-    def test_str(self):
-        """test that the str method has the correct output"""
-        state = State()
-        string = "[State] ({}) {}".format(state.id, state.__dict__)
-        self.assertEqual(string, str(state))
+    def test_to_dict_state(self):
+        """ test State.to_dict() """
+        self.s.name = "NYC"
+        dict1 = self.s.to_dict()
+
+        """ confirming the type of each attr in dict """
+        self.assertEqual(type(dict1['name']), str)
+        self.assertEqual(type(dict1['__class__']), str)
+        self.assertEqual(dict1['__class__'], "State")
+        self.assertEqual(type(dict1['updated_at']), str)
+        self.assertEqual(type(dict1['id']), str)
+        self.assertEqual(type(dict1['created_at']), str)
+
+        """ test positional args """
+        with self.assertRaises(TypeError):
+            self.s.to_dict('str')
+
+
+if __name__ == '__main__':
+    unittest.main()
