008b9afb3ca8474d6b9b378fa1deea535255b029
Sorady <125706196+Sorady@users.noreply.github.com>
2023-08-13 05:22:28 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-# Alex-reid and Joyclare
-
-"""New class inherit from BaseModel"""
+"""Contains the user class"""
+import models
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Class User that inherit from BaseModel"""
+    """Class User that inherits from BaseModel
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    Args:
+        BaseModel (_type_): Class BaseModel
+    """
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
 
     def __init__(self, *args, **kwargs):
-        """Initialize constructor for BaseModel"""
+        """Initialization of the public atributes
+        """
         super().__init__(*args, **kwargs)
