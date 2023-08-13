44b19bf44f66aec56c55ff963d73ce560e3962ad
Sorady <125706196+Sorady@users.noreply.github.com>
2023-08-13 03:13:42 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,11 @@
-#!/usr/bin/python3
-"""This is the Amenity Model module.
-
-Contains the Amenity class that inherits from BaseModel.
-"""
+#!/user/bin/python3
+""" aminity class """
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """This class defines an Amenity.
-
-    Attributes:
-        name (str): the amenity's name.
+    """ class aminity inherits from BaseModel
+        Args:
+            name: string - empty string
     """
-
     name = ""
